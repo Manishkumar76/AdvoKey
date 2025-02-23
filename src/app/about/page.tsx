900861185217cc353c1c9b1c1935e871f93f2711
Manishkumar76 <mkshakya794@gmail.com>
2025-02-23 19:57:04 +0530

    dynamic_routing

---
+++ src/app/about/page.tsx
@@ -0,0 +1,7 @@
+export default function About() {
+    return (
+        <div className="h-full w-full">
+        <h1>About Page</h1>
+        </div>
+    );
+    }
---
+++ src/app/dynamicRoute/page.tsx
@@ -0,0 +1,12 @@
+
+export default function Page() {
+    
+    return (
+        <div className="bg-white h-full w-full">
+            <h1>Dymanic Routes</h1>
+            <h2>Route 1</h2>
+            <h2>Route 2</h2>
+            <h2>Route 3</h2>
+        </div>
+    );
+    }
